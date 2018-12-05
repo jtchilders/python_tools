c995e793beab702d00db628bd57ea2d650a9dfe8
Taylor Childers <jchilders@alcfwl059.alcf.anl.gov>
2018-12-05 09:37:54 -0600

    updating app_dummy to use arg parse

--- app_dummy.py
+++ app_dummy.py
@@ -1,25 +1,60 @@
 #!/usr/bin/env python
-import os,sys,optparse,logging
+import argparse,logging
 logger = logging.getLogger(__name__)
+
 
 def main():
    ''' simple starter program that can be copied for use when starting a new script. '''
-   logging.basicConfig(level=logging.INFO,format='%(asctime)s %(levelname)s:%(name)s:%(message)s')
+   logging_format = '%(asctime)s %(levelname)s:%(name)s:%(message)s'
+   logging_datefmt = '%Y-%m-%d %H:%M:%S'
+   logging.basicConfig(level=logging.INFO,format=logging_format,datefmt=logging_datefmt)
 
-   parser = optparse.OptionParser(description='')
-   parser.add_option('-i','--input',dest='input',help='input')
-   options,args = parser.parse_args()
+   parser = argparse.ArgumentParser(description='')
+   parser.add_argument('-i','--input',dest='input',help='input',required=True)
+   parser.add_argument('--debug', dest='debug', default=False, action='store_true', help="Set Logger to DEBUG")
+   parser.add_argument('--error', dest='error', default=False, action='store_true', help="Set Logger to ERROR")
+   parser.add_argument('--warning', dest='warning', default=False, action='store_true', help="Set Logger to ERROR")
+   parser.add_argument('--logfilename',dest='logfilename',default=None,help='if set, logging information will go to file')
+   args = parser.parse_args()
+
+   if args.debug and not args.error and not args.warning:
+      # remove existing root handlers and reconfigure with DEBUG
+      for h in logging.root.handlers:
+         logging.root.removeHandler(h)
+      logging.basicConfig(level=logging.DEBUG,
+                          format=logging_format,
+                          datefmt=logging_datefmt,
+                          filename=args.logfilename)
+      logger.setLevel(logging.DEBUG)
+   elif not args.debug and args.error and not args.warning:
+      # remove existing root handlers and reconfigure with ERROR
+      for h in logging.root.handlers:
+         logging.root.removeHandler(h)
+      logging.basicConfig(level=logging.ERROR,
+                          format=logging_format,
+                          datefmt=logging_datefmt,
+                          filename=args.logfilename)
+      logger.setLevel(logging.ERROR)
+   elif not args.debug and not args.error and args.warning:
+      # remove existing root handlers and reconfigure with WARNING
+      for h in logging.root.handlers:
+         logging.root.removeHandler(h)
+      logging.basicConfig(level=logging.WARNING,
+                          format=logging_format,
+                          datefmt=logging_datefmt,
+                          filename=args.logfilename)
+      logger.setLevel(logging.WARNING)
+   else:
+      # set to default of INFO
+      for h in logging.root.handlers:
+         logging.root.removeHandler(h)
+      logging.basicConfig(level=logging.INFO,
+                          format=logging_format,
+                          datefmt=logging_datefmt,
+                          filename=args.logfilename)
+      logger.info('Start yoda_droid: %s',__file__)
 
    
-   manditory_args = [
-                     'input',
-                  ]
-
-   for man in manditory_args:
-      if man not in options.__dict__ or options.__dict__[man] is None:
-         logger.error('Must specify option: ' + man)
-         parser.print_help()
-         sys.exit(-1)
    
 
 
